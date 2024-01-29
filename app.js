55430d80c483afdc4ac1bb4f70a57619b15ea1c8
Jamal Karim <1jamalkarim@gmail.com>
2024-01-29 07:26:23 -0800

    add start ts file for testing

---
+++ app.js
@@ -0,0 +1,7 @@
+var money = 34;
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+console.log(money);
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ app.ts
@@ -0,0 +1,11 @@
+const money = 34;
+
+
+const inputs = document.querySelectorAll('input');
+
+console.log(inputs);
+console.log(money);
+
+inputs.forEach(function(input) {
+    console.log(input);
+})
